2a24ed7153e4d94467ad4e12506659ae85405667
Abhijith Appukuttan <abhijith.kesavan@kpsbremen.de>
2021-06-17 21:41:51 +0200

    Add job labeler microservice

---
+++ labeler/src/test/java/com/example/labelapi/LabellngJobApiAppTests.java
@@ -0,0 +1,13 @@
+package com.example.labelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabellngJobApiAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
